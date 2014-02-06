406023e99e00270c614b3d884a20aa557ac17a3f
Ryan Yu <ryanyu399@yahoo.com>
2014-02-06 13:16:32 -0800

    lab5

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,11 @@ exports.view = function(req, res){
 	console.log(data);
 	res.render('index');
 };
+
+exports.view = function(req, res){
+  res.render('index', {
+    'name': 'Fake Friend',
+    'image': 'http://lorempixel.com/500/500/people',
+    'id': 'pic'
+  });
+};
